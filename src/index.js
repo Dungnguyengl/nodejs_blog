fa39022f4e38ebbfe0f6788751a215b881c74512
Dungnguyengl <nguyendungglk3@gmail.com>
2021-08-19 15:02:51 +0700

    update template

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars');
+const app = express();
+const port = 8080;
+
+// HTTP logger
+app.use(morgan('dev'));
+
+// template eng
+
+app.engine('.hbs', exphbs({
+    layoutsDir: __dirname + '\\resources\\view\\layouts',
+    extname: '.hbs'
+}));
+app.set('views', path.join(__dirname, 'resources\\view'));
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/new', (req, res) => {
+    res.render('new');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
